f96074cf20d0b8894c13083d6abe344a796af6bb
arkadiusz.chrabaszczewski@deva.as <arkadiusz.chrabaszczewski@deva.as>
2019-10-11 12:32:32 +0200

    fix SetData stack

--- Mehdime.Entity/Remoting/CallContext.cs
+++ Mehdime.Entity/Remoting/CallContext.cs
@@ -8,10 +8,9 @@ namespace Remoting
         static ConcurrentDictionary<string, AsyncLocal<T>> state = new ConcurrentDictionary<string, AsyncLocal<T>>();
 
         public static void SetData(string name, T data) =>
-            state.AddOrUpdate(name, new AsyncLocal<T>(), (_, __) => new AsyncLocal<T>()).Value = data;
-            // state.GetOrAdd(name, _ => new AsyncLocal<T>()).Value = data;
+            state.GetOrAdd(name, _ => new AsyncLocal<T>()).Value = data;
 
         public static T GetData(string name) =>
-            state.TryGetValue(name, out AsyncLocal<T> data) ? data.Value : default(T);
+            state.TryGetValue(name, out AsyncLocal<T> data) ? data.Value : default;
     }
 }
